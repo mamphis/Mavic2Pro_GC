6bd2ff1d957dd5772bc1b691304b288c082fa691
Mattis Wacker <mattis.wacker@gmx.de>
2022-05-17 11:34:45 +0200

    Initial commit
    - Added LoginPage
    - Successfully register the application
    - Implemented first navigation

---
+++ Mavic2Pro_GC/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using DJI.WindowsSDK;
+using Mavic2Pro_GC.View;
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+namespace Mavic2Pro_GC
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly Dictionary<string, Type> navigationItems = new Dictionary<string, Type>()
+        {
+            {"Login", typeof(UserLogin) }
+        };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void FlightControllerHandler_FlightTimeInSecondsChanged(object sender, IntMsg? value)
+        {
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            DJISDKManager.Instance.SDKRegistrationStateChanged += this.Instance_SDKRegistrationEvent;
+
+            DJISDKManager.Instance.RegisterApp("<key>");
+        }
+
+        private async void Instance_SDKRegistrationEvent(SDKRegistrationState state, SDKError resultCode)
+        {
+            if (resultCode == SDKError.NO_ERROR)
+            {
+                System.Diagnostics.Debug.WriteLine("Register app successfully.");
+                await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+
+                    foreach (KeyValuePair<string, Type> navItem in this.navigationItems)
+                    {
+                        this.NavView.MenuItems.Add(navItem.Key);
+                    }
+
+                    this.NavView.UpdateLayout();
+                });
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Register SDK failed, the error is: ");
+                System.Diagnostics.Debug.WriteLine(resultCode.ToString());
+            }
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            string invokedItem = args.InvokedItem as string;
+            if (this.navigationItems.ContainsKey(invokedItem))
+            {
+                Type page = this.navigationItems[invokedItem];
+                if (this.ContentFrame.SourcePageType != page)
+                {
+                    this.ContentFrame.Navigate(page);
+                }
+            }
+        }
+    }
+}
---
+++ Mavic2Pro_GC/View/UserLogin.xaml.cs
@@ -0,0 +1,37 @@
+﻿// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace Mavic2Pro_GC.View
+{
+    using DJI.WindowsSDK;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.InteropServices.WindowsRuntime;
+    using Windows.Foundation;
+    using Windows.Foundation.Collections;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Controls.Primitives;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Navigation;
+
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class UserLogin : Page
+    {
+        public UserLogin()
+        {
+            this.InitializeComponent();
+
+            var loginWebView = DJISDKManager.Instance.UserAccountManager.CreateLoginView(false);
+            if (contentGrid.Children.Count == 0)
+            {
+                contentGrid.Children.Add(loginWebView);
+            }
+        }
+    }
+}
