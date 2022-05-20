7ecf87efc5fe928985f3568cff3b92ed11ec422f
RazorZorax <henry.wiesner7@gmail.com>
2022-05-20 21:12:54 +0200

    VideoFeed Implemantation

---
+++ Mavic2Pro_GC/View/LiveVideoFeed.xaml.cs
@@ -0,0 +1,89 @@
+﻿using DJI.WindowsSDK;
+using DJIVideoParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Mavic2Pro_GC.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LiveVideoFeed : Page
+    {
+        public LiveVideoFeed()
+        {
+            this.InitializeComponent();
+        }
+        private DJIVideoParser.Parser videoParser;
+        //raw data
+        void OnVideoPush(VideoFeed sender, byte[] bytes)
+        {
+            videoParser.PushVideoData(0, 0, bytes, bytes.Length);
+        }
+
+        //Decode data. Do nothing here. This function would return a bytes array with image data in RGBA format.
+        async void ReceiveDecodedData(byte[] data, int width, int height)
+        {
+        }
+
+        //We need to set the camera type of the aircraft to the DJIVideoParser. After setting camera type, DJIVideoParser would correct the distortion of the video automatically.
+        private void OnCameraTypeChanged(object sender, CameraTypeMsg? value)
+        {
+            if (value != null)
+            {
+                switch (value.Value.value)
+                {
+                    case CameraType.MAVIC_2_ZOOM:
+                        this.videoParser.SetCameraSensor(AircraftCameraType.Mavic2Zoom);
+                        break;
+                    case CameraType.MAVIC_2_PRO:
+                        this.videoParser.SetCameraSensor(AircraftCameraType.Mavic2Pro);
+                        break;
+                    default:
+                        this.videoParser.SetCameraSensor(AircraftCameraType.Others);
+                        break;
+                }
+
+            }
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+            {
+                //Raw data and decoded data listener
+                if (videoParser == null)
+                {
+                    videoParser = new DJIVideoParser.Parser();
+                    videoParser.Initialize(delegate (byte[] data)
+                    {
+                        //Note: This function must be called because we need DJI Windows SDK to help us to parse frame data.
+                        return DJISDKManager.Instance.VideoFeeder.ParseAssitantDecodingInfo(0, data);
+                    });
+                    //Set the swapChainPanel to display and set the decoded data callback.
+                    videoParser.SetSurfaceAndVideoCallback(0, 0, swapChainPanel, ReceiveDecodedData);
+                    DJISDKManager.Instance.VideoFeeder.GetPrimaryVideoFeed(0).VideoDataUpdated += OnVideoPush;
+                }
+                //get the camera type and observe the CameraTypeChanged event.
+                DJISDKManager.Instance.ComponentManager.GetCameraHandler(0, 0).CameraTypeChanged += OnCameraTypeChanged;
+                var type = await DJISDKManager.Instance.ComponentManager.GetCameraHandler(0, 0).GetCameraTypeAsync();
+                OnCameraTypeChanged(this, type.value);
+            });
+        }
+    }
+}
